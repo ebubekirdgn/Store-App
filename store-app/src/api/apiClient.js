345d09c6e6560c5fa19b0c5c8a3b689efa157d6b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-05-14 11:01:50 +0300

    add interceptors

--- store-app/src/api/apiClient.js
+++ store-app/src/api/apiClient.js
@@ -2,6 +2,18 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:5000/";
 
+axios.interceptors.response.use(
+  (response) => {
+    console.log("success");
+    return response;
+  },
+  (error) => {
+    console.log("error...");
+    console.log(error);
+    return Promise.reject(error.message);
+  }
+);
+
 const methods = {
   get: (url) => axios.get(url).then((response) => response.data),
   post: (url, body) => axios.post(url, body).then((response) => response.data),
