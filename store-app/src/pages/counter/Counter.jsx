e2ad3dc243ce31980cb5b2c19e895bc282c37b18
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-05-19 05:25:19 +0300

    add Counter

--- store-app/src/pages/counter/Counter.jsx
+++ store-app/src/pages/counter/Counter.jsx
@@ -1,10 +1,10 @@
 import { Button, ButtonGroup, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment, incrementByValue } from "./counterSlice";
+import { decrement, increment, incrementByAmount } from "../counter/counterSlice";
 
 export default function Counter() {
-  const value = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
+  const value = useSelector((state) => state.counter.value); // Redux store'dan state verisi okumak için kullanılır.  
+  const dispatch = useDispatch(); // Redux store'a action göndermek için kullanılır. 
 
   return (
     <>
@@ -12,7 +12,7 @@ export default function Counter() {
       <ButtonGroup>
         <Button onClick={() => dispatch(increment())}>Increment</Button>
         <Button onClick={() => dispatch(decrement())}>Decrement</Button>
-        <Button onClick={() => dispatch(incrementByValue(5))}>
+        <Button onClick={() => dispatch(incrementByAmount(5))}>
           Increment By Value
         </Button>
       </ButtonGroup>
--- store-app/src/pages/home/Home.jsx
+++ store-app/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
  import React from 'react'
+import Counter from '../counter/Counter'
  
  function HomePage() {
    return (
-     <div>HomePage</div>
+     <div>
+      <Counter />
+     </div>
    )
  }
  
