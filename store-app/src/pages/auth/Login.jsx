981a9bca0315498a07b5724650a72d2c3d8ef584
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-05-21 00:44:18 +0300

    add react-hook-form

--- store-app/src/pages/auth/Login.jsx
+++ store-app/src/pages/auth/Login.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 function LoginPage() {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(values);
+  }
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  }
+
   return (
-    <div>LoginPage</div>
-  )
+    <Container maxWidth="xs">
+      <Paper sx={{ padding: 2 }} elevation={3}>
+        <Avatar sx={{ mx: "auto", mb: 2, color: "primary.main" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography
+          component="h1"
+          variant="h5"
+          sx={{ textAlign: "center", mb: 2 }}
+        >
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mb: 2 }}>
+          <TextField
+            value={values.username}
+            // onChange={(e) => setUsername(e.target.value)}
+            onChange={handleInputChange}
+            name="username"
+            label="Enter username"
+            size="small"
+            fullWidth
+            required
+            autoFocus
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            value={values.password}
+            onChange={handleInputChange}
+            name="password"
+            type="password"
+            label="Enter password"
+            size="small"
+            fullWidth
+            required
+            sx={{ mb: 2 }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{ mt: 1 }}
+            color="primary"
+          >
+            Submit
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
