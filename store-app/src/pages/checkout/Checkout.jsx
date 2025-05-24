17fe5b5ae1cc33b1b4458e16017023463603a14d
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-05-24 17:00:39 +0300

    add checkout page

--- store-app/src/pages/checkout/Checkout.jsx
+++ store-app/src/pages/checkout/Checkout.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Container, Grid, Paper, Typography, Stack, Divider, Card, CardContent } from "@mui/material";
+import Info from "./Info";
+import AddressForm from "./AddressForm";
+import PaymentForm from "./PaymentForm";
+import Review from "./Review";
 
 function CheckoutPage() {
   return (
-    <div>CheckoutPage</div>
-  )
+    <Container maxWidth="lg" sx={{ py: 5 }}>
+      <Typography variant="h4" fontWeight={700} gutterBottom>
+        Ödeme Sayfası
+      </Typography>
+      <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+        Siparişinizi tamamlamak için lütfen bilgilerinizi doldurun.
+      </Typography>
+      <Grid container spacing={4} sx={{ mt: 2 }}>
+        <Grid item xs={12} md={4}>
+          <Card elevation={3} sx={{ borderRadius: 3 }}>
+            <CardContent>
+              <Info />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Paper elevation={2} sx={{ p: 3, borderRadius: 3 }}>
+            <Stack spacing={3}>
+              <AddressForm />
+              <Divider />
+              <PaymentForm />
+              <Divider />
+              <Review />
+            </Stack>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default CheckoutPage
+export default CheckoutPage;
