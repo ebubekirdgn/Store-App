92fe24594983d406e5a1a042fc03dd87cacd8706
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-05-25 00:05:53 +0300

    add Review content

--- store-app/src/pages/checkout/Review.jsx
+++ store-app/src/pages/checkout/Review.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Box, Divider, Stack, Typography } from "@mui/material";
+import { useFormContext } from "react-hook-form";
 
-function Review() {
+export default function Review() {
+  const { getValues } = useFormContext();
   return (
-    <div>Review</div>
-  )
-}
+    <Stack spacing={2} sx={{ mb: 3 }} divider={<Divider />}>
+      <Box>
+        <Typography variant="subtitle2" gutterBottom>
+          Teslimat Bilgileri
+        </Typography>
+        <Typography gutterBottom>
+          {getValues("firstname")} {getValues("lastname")}
+        </Typography>
+        <Typography gutterBottom>{getValues("phone")}</Typography>
+        <Typography gutterBottom>
+          {getValues("address")} / {getValues("city")}
+        </Typography>
+      </Box>
 
-export default Review
+      <Box>
+        <Typography variant="subtitle2" gutterBottom>
+          Ödeme Bilgileri
+        </Typography>
+        <Typography gutterBottom>{getValues("cardname")}</Typography>
+        <Typography gutterBottom>{getValues("cardnumber")}</Typography>
+        <Typography gutterBottom>{getValues("expirydate")}</Typography>
+      </Box>
+    </Stack>
+  );
+}
