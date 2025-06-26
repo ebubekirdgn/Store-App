0c7fb0ddc04f11ee15bd424256fd5281c1463668
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2025-06-26 19:50:15 +0300

    Initial commit
    Add initial project files and setup.

--- store-api/app.js
+++ store-api/app.js
@@ -28,9 +28,13 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4173");
+  const allowedOrigins = ["http://localhost:4173", "http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
